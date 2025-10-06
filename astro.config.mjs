ce0ccf5310825825ab20cde5c2ec4662d8ad9f62
Rui Vilela <tek@qwertytek.com>
2025-10-06 15:35:28 +0100

    change structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
-
+import { loadEnv } from "vite";
 import tailwindcss from '@tailwindcss/vite';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
-// https://astro.build/config
+const { SECRET_SANITY_TOKEN } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+
+console.log(SECRET_SANITY_TOKEN);
+
 export default defineConfig({
   adapter: vercel(),
-
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    sanity({
+      projectId: "6yzu93tt",
+      dataset: 'production',
+      useCdn: false,
+      token: SECRET_SANITY_TOKEN
+    }),
+    react()
+  ]
 });
